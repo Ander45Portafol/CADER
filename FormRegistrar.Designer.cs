0d46b680f235c66bcb2caecf4f5acfccd8903af1
Alfonsotrs <jplaterotrigueros@gmail.com>
2025-05-10 17:41:00 -0600

    Add files via upload

---
+++ FormRegistrar.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace CADER
+{
+    partial class FormRegistrar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtServicio = new System.Windows.Forms.TextBox();
+            this.txtDui = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtclave2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(38, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(353, 23);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Pastor, Administración, Rececpción";
+            // 
+            // txtServicio
+            // 
+            this.txtServicio.BackColor = System.Drawing.Color.LightGray;
+            this.txtServicio.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtServicio.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtServicio.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtServicio.Location = new System.Drawing.Point(42, 428);
+            this.txtServicio.Name = "txtServicio";
+            this.txtServicio.Size = new System.Drawing.Size(343, 29);
+            this.txtServicio.TabIndex = 22;
+            // 
+            // txtDui
+            // 
+            this.txtDui.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.txtDui.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDui.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDui.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtDui.Location = new System.Drawing.Point(45, 524);
+            this.txtDui.Name = "txtDui";
+            this.txtDui.Size = new System.Drawing.Size(124, 29);
+            this.txtDui.TabIndex = 21;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.txtTelefono.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTelefono.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefono.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtTelefono.Location = new System.Drawing.Point(238, 524);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(132, 29);
+            this.txtTelefono.TabIndex = 20;
+            // 
+            // txtClave
+            // 
+            this.txtClave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.txtClave.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtClave.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClave.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtClave.Location = new System.Drawing.Point(45, 199);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(343, 29);
+            this.txtClave.TabIndex = 19;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNombre.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtNombre.Location = new System.Drawing.Point(45, 82);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(343, 29);
+            this.txtNombre.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(34, 355);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 27);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Servicio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 484);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 27);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "DUI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(231, 484);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 27);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Teléfono";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 27);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Contraseña:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 27);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Usuario:";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnRegistrar.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold);
+            this.btnRegistrar.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnRegistrar.Location = new System.Drawing.Point(827, 579);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(284, 56);
+            this.btnRegistrar.TabIndex = 24;
+            this.btnRegistrar.Text = "Continuar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtclave2
+            // 
+            this.txtclave2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.txtclave2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtclave2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtclave2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtclave2.Location = new System.Drawing.Point(42, 297);
+            this.txtclave2.Name = "txtclave2";
+            this.txtclave2.Size = new System.Drawing.Size(343, 29);
+            this.txtclave2.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(37, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(264, 27);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Confirmar Contraseña:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(441, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(215, 44);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "¡Regístrate!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CADER.Properties.Resources.Mobile_login_rafiki_1;
+            this.pictureBox1.Location = new System.Drawing.Point(585, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(460, 460);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 28;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormRegistrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            this.ClientSize = new System.Drawing.Size(1123, 647);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtclave2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtServicio);
+            this.Controls.Add(this.txtDui);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormRegistrar";
+            this.Text = "FormRegistrar";
+            this.Load += new System.EventHandler(this.FormRegistrar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtServicio;
+        private System.Windows.Forms.TextBox txtDui;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtclave2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ FormRegistrar.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CADER
+{
+    public partial class FormRegistrar: Form
+    {
+        public FormRegistrar()
+        {
+            InitializeComponent();
+        }
+
+        public void limpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtClave.Text = "";
+            txtclave2.Text = "";
+            txtDui.Text = "";
+            txtTelefono.Text = "";
+            txtServicio.Text = "";
+        }
+        private void GuardarDatos()
+        {
+            // Cadena de conexión a la base de datos con autenticación de Windows
+            string connectionString = "Data Source=.;Initial Catalog=CADER;Integrated Security=True";
+
+            // Consulta SQL para insertar los datos
+            string query = "INSERT INTO Usuarios (id_iglesia, nombre_usuario, clave_usuario, telefono_usuario, dui_usuario, tipo_usuario) " +
+                           "VALUES (@Iglesia, @Nombre, @Clave, @Telefono, @Dui, @Tipo)";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        // Asignar valores a los parámetros
+                        command.Parameters.AddWithValue("@Iglesia", ObtenerIdIglesia()); // Método para obtener el valor de id_iglesia
+                        command.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        command.Parameters.AddWithValue("@Clave", txtClave.Text);
+                        command.Parameters.AddWithValue("@Telefono", txtTelefono.Text);
+                        command.Parameters.AddWithValue("@Dui", txtDui.Text);
+                        command.Parameters.AddWithValue("@Tipo", txtServicio.Text);
+
+                        // Abrir la conexión
+                        connection.Open();
+
+                        // Ejecutar el comando
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        // Verificar si se insertaron los datos
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Datos guardados correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            limpiarCampos();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se pudieron guardar los datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private int ObtenerIdIglesia()
+        {
+            // Aquí puedes obtener el valor de id_iglesia desde un control como un ComboBox o asignar un valor fijo
+            // Por ejemplo, si tienes un ComboBox llamado cmbIglesia:
+            // return Convert.ToInt32(cmbIglesia.SelectedValue);
+
+            // Si es un valor fijo, puedes devolverlo directamente:
+            return 1; // Cambia este valor según sea necesario
+        }
+
+        private void FormRegistrar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (txtClave.Text == txtclave2.Text)
+            {
+                GuardarDatos();
+            }
+            else
+            {
+                MessageBox.Show("Las contraseñas no coinciden. Por favor, verifícalas.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
